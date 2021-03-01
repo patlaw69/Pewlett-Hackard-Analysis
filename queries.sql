d2a03b62cf492fc1e6df2cb531d905bef582ab80
Pat Lawrence <75762456+patlaw69@users.noreply.github.com>
2021-02-28 20:15:33 -0600

    Adding "queries.sql" and "updating retirement_info.csv"

--- queries.sql
+++ queries.sql
@@ -7,23 +7,23 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE manager (
+CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -35,36 +35,30 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-DROP TABLE dept_employees;
-
 CREATE TABLE dept_emp (
- 	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
- 	to_date DATE NOT NULL,
- 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
-	emp_no INT NOT NULL,
- 	title VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
- 	PRIMARY KEY (emp_no, title, from_date)
+  emp_no INT NOT NULL,
+  title VARCHAR (40) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, title, from_date)
 );
 
-SELECT * FROM dept_employees;
+SELECT * FROM departments;
 
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
-
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'
 
 SELECT first_name, last_name
 FROM employees
@@ -84,7 +78,7 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
--- Retirement eligibility
+-- Number of employees retiring
 SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
